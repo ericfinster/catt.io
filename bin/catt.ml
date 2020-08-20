a2564497ffb30d984739c58922d56e9cf6d6a2cb
Eric Finster <ericfinster@gmail.com>
2020-08-20 15:59:51 +0200

    toplevel

--- bin/catt.ml
+++ bin/catt.ml
@@ -2,54 +2,54 @@
  * catt.ml - top level catt module
  *)
 
-(* open Catt.Expr
- * open Catt.Typecheck
- * open Cheshire.Err
- *        
- * open TcmMonad
- * 
- * let usage = "catt [options] [file]"
- * 
- * let parse s =
- *   let lexbuf = Lexing.from_string s in
- *   try
- *     Catt.Parser.prog Catt.Lexer.token lexbuf
- *   with
- *   | Failure msg -> print_endline msg; exit (-1)
- *   | Parsing.Parse_error -> print_endline "Parse error"; exit (-1)
- * 
- * let parse_file f =
- *   let sin =
- *     let fi = open_in f in
- *     let flen = in_channel_length fi in
- *     let buf = Bytes.create flen in
- *     really_input fi buf 0 flen;
- *     close_in fi;
- *     buf
- *   in parse (Bytes.to_string sin)
- *                            
- * let rec tc_check_all files =
- *   match files with
- *   | [] -> tc_get_env
- *   | f::fs ->
- *      (\* let s = Node.Fs.readFileAsUtf8Sync f in *\)
- *      let (impts, cmds) = parse_file f in
- *      let imprt_nms = List.map (fun fn -> fn ^ ".catt") impts in 
- *      tc_check_all imprt_nms >>= fun env -> 
- *      Printf.printf "-----------------\n";
- *      Printf.printf "Processing input file: %s\n" f;
- *      tc_with_env env (check_cmds cmds) >>= fun env' ->
- *      tc_with_env env' (tc_check_all fs) *)
+open Catt.Term
+open Catt.Command
+       
+open Cheshire.Err
+       
+open TcMonad.MonadSyntax
+
+let usage = "catt [options] [file]"
+
+let parse s =
+  let lexbuf = Lexing.from_string s in
+  try
+    Catt.Parser.prog Catt.Lexer.token lexbuf
+  with
+  | Failure msg -> print_endline msg; exit (-1)
+  | Parsing.Parse_error -> print_endline "Parse error"; exit (-1)
+
+let parse_file f =
+  let sin =
+    let fi = open_in f in
+    let flen = in_channel_length fi in
+    let buf = Bytes.create flen in
+    really_input fi buf 0 flen;
+    close_in fi;
+    buf
+  in parse (Bytes.to_string sin)
+                           
+let rec tc_check_all files =
+  match files with
+  | [] -> tc_env
+  | f::fs ->
+     (* let s = Node.Fs.readFileAsUtf8Sync f in *)
+     let (impts, cmds) = parse_file f in
+     let imprt_nms = List.map (fun fn -> fn ^ ".catt") impts in
+     let* env = tc_check_all imprt_nms in 
+     Printf.printf "-----------------\n";
+     Printf.printf "Processing input file: %s\n" f;
+     let* env' = tc_with_env env (check_cmds cmds) in 
+     tc_with_env env' (tc_check_all fs)
      
 let () =
-  Printf.printf "Catt is kinda broken right now ..."
-  (* let file_in = ref []
-   * in Arg.parse [] (fun s -> file_in := s::!file_in) usage;
-   *    let files = List.rev (!file_in) in 
-   *    (\* let files = List.tl (List.rev (!file_in)) in *\)
-   *    match tc_check_all files empty_env with
-   *    | Ok _ -> print_endline "-----------------\nFinished"
-   *    | Fail msg -> Printf.printf "Typechecking error: %s\n" msg ; exit (-1) *)
+  let file_in = ref []
+  in Arg.parse [] (fun s -> file_in := s::!file_in) usage;
+     let files = List.rev (!file_in) in 
+     (* let files = List.tl (List.rev (!file_in)) in *)
+     match tc_check_all files empty_env with
+     | Ok _ -> print_endline "-----------------\nFinished"
+     | Fail msg -> Printf.printf "Typechecking error: %s\n" msg ; exit (-1)
        
      
 
--- lib/command.ml
+++ lib/command.ml
@@ -5,7 +5,12 @@
 (*****************************************************************************)
 
 open Expr
+open Term
 
+open Printf
+
+open TcMonad.MonadSyntax
+       
 type cmd =
   | CellDef of string * tele * ty_expr
   | TermDef of string * tele * ty_expr * tm_expr
@@ -16,3 +21,26 @@ type cmd =
    * | Normalize of tele * tm_expr
    * | LocMax of tele *)
 
+let rec check_cmds cmds =
+  match cmds with
+  | [] -> tc_env 
+  | (CellDef (id, _, _)) :: ds ->
+     printf "-----------------\n";
+     printf "Checking cell: %s\n" id;
+     (* tc_check_cell_expr cell >>= fun cell_tm ->
+      * printf "Ok!\n";
+      * tc_with_cell id cell_tm (check_cmds ds) *)
+     let* env = check_cmds ds in 
+     tc_ok env
+  | (TermDef (id, _, _, _)) :: ds ->
+     printf "-----------------\n";
+     printf "Checking definition: %s\n" id;
+     (* tc_check_tele tele >>= fun g ->
+      * printf "Valid telescope: %s\n" (print_term_ctx g);
+      * tc_in_ctx g (tc_check_ty_expr typ) >>= fun typ_tm ->
+      * printf "Valid return type: %s\n" (print_ty_term typ_tm);
+      * tc_in_ctx g (tc_check_tm_expr tm typ_tm) >>= fun tm_tm ->
+      * printf "Valid definition: %s\n" (print_tm_term tm_tm);
+      * tc_with_def id g typ_tm tm_tm (check_cmds ds) *)
+     let* env = check_cmds ds in
+     tc_ok env 
