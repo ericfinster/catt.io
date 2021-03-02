e3055c3bb0c057b0227d5fe7088c503a22e1b28d
Eric Finster <ericfinster@gmail.com>
2021-03-02 22:31:03 +0100

    show implicits flag

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -61,39 +61,46 @@ let pp_tele pp_el ppf tl =
     | Impl -> pf ppf "{%s : %a}" nm pp_el t
   in pp_suite pp_trpl ppf tl 
 
-let rec pp_expr ppf expr =
+let rec pp_expr_gen show_imp ppf expr =
+  let ppe = pp_expr_gen show_imp in 
   match expr with
   | VarE nm -> string ppf nm
-  | LamE (nm,Impl,bdy) -> pf ppf "\\{%s}. %a" nm pp_expr bdy
-  | LamE (nm,Expl,bdy) -> pf ppf "\\%s. %a" nm pp_expr bdy
+  | LamE (nm,Impl,bdy) -> pf ppf "\\{%s}. %a" nm ppe bdy
+  | LamE (nm,Expl,bdy) -> pf ppf "\\%s. %a" nm ppe bdy
   | AppE (u, v, Impl) ->
-    pf ppf "%a {%a}" pp_expr u pp_expr v
+    if show_imp then 
+      pf ppf "%a {%a}" ppe u ppe v
+    else
+      pf ppf "%a" ppe u 
   | AppE (u, v, Expl) ->
     let pp_v = if (is_app v) then
-        parens pp_expr
-      else pp_expr in 
-    pf ppf "%a %a" pp_expr u pp_v v
+        parens ppe
+      else ppe in 
+    pf ppf "%a %a" ppe u pp_v v
   | PiE (nm,Impl,dom,cod) ->
     pf ppf "{%s : %a} -> %a" nm
-      pp_expr dom pp_expr cod
+      ppe dom ppe cod
   | PiE (nm,Expl,a,b) when Poly.(=) nm "" ->
     let pp_a = if (is_pi a) then
-        parens pp_expr
-      else pp_expr in 
+        parens ppe
+      else ppe in 
     pf ppf "%a -> %a" 
-      pp_a a pp_expr b
+      pp_a a ppe b
   | PiE (nm,Expl,dom,cod) ->
     pf ppf "(%s : %a) -> %a" nm
-      pp_expr dom pp_expr cod
-  | ObjE e -> pf ppf "[%a]" pp_expr e
+      ppe dom ppe cod
+  | ObjE e -> pf ppf "[%a]" ppe e
   | HomE (c,s,t) ->
-    pf ppf "%a | %a => %a" pp_expr c pp_expr s pp_expr t
+    pf ppf "%a | %a => %a" ppe c ppe s ppe t
   | CohE (g,a) ->
-    pf ppf "coh @[[ %a : %a ]@]" (pp_tele pp_expr) g pp_expr a
+    pf ppf "coh @[[ %a : %a ]@]" (pp_tele ppe) g ppe a
   | CatE -> string ppf "Cat"
   | TypE -> string ppf "U"
   | HoleE -> string ppf "_"
-  
+
+let pp_expr = pp_expr_gen false
+let pp_expr_with_impl = pp_expr_gen true
+    
 (*****************************************************************************)
 (*                              Internal Syntax                              *)
 (*****************************************************************************)
