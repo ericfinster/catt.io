b2cf23dc063de224f66c079d882820cf24a8c620
regular-citizen <chiara@sarti.me>
2023-05-29 12:39:35 +0100

    Added support for strictification modes.

--- web/cattweb.ml
+++ web/cattweb.ml
@@ -18,10 +18,24 @@ let () =
     (object%js
       method parse s =
           s |> Js.to_string |> parse_stringify |> Js.string
-      method typecheck_weak s =
+      method typecheckweak s =
           let r = try
               let defs = parse_string (Js.to_string s) in
               let res = W.print_tc (W.check_defs W.empty_ctx defs) in
+              res
+          with _ -> Printf.sprintf "Failure" in
+          Js.string r
+      method typechecksu s =
+          let r = try
+              let defs = parse_string (Js.to_string s) in
+              let res = SU.print_tc (SU.check_defs SU.empty_ctx defs) in
+              res
+          with _ -> Printf.sprintf "Failure" in
+          Js.string r
+      method typechecksua s =
+          let r = try
+              let defs = parse_string (Js.to_string s) in
+              let res = SUA.print_tc (SUA.check_defs SUA.empty_ctx defs) in
               res
           with _ -> Printf.sprintf "Failure" in
           Js.string r
