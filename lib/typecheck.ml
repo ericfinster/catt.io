38cfe7cf20388fc60d459378af95cc0925f67f75
regular-citizen <chiara@sarti.me>
2023-05-30 12:10:15 +0100

    Wired typechecker output to editor panel.

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -534,14 +534,7 @@ let rec check_defs gma defs =
 let run_tc m =
   match m with
   | Ok _ ->
-    Fmt.pr "@[<v>----------------@,Success!@,@,@]"
+    Fmt.pr "@[<v>----------------@,Success!@,@,@]%!"
   | Error err ->
-     Fmt.pr "@,Typing error: @,@,%a@,@," pp_error err
-
-let print_tc m =
-  match m with
-  | Ok _ ->
-    Printf.sprintf "@[<v>----------------@,Typecheck Success!@,@,@]"
-  | Error err ->
-    Printf.sprintf "@[<v>----------------@,Typecheck Failure!@,@,@]"
+     Fmt.pr "@,Typing error: @,@,%a@,@,%!" pp_error err
 end
--- web/cattweb.ml
+++ web/cattweb.ml
@@ -13,31 +13,30 @@ module W = Catt.Typecheck.Make(Weak)
 module SU = Catt.Typecheck.Make(StrictUnit.Rec)
 module SUA = Catt.Typecheck.Make(StrictUnitAssoc.Rec)
 
+let print_to_js s : unit =
+    let s = Js.string s in
+    Js.Unsafe.fun_call (Js.Unsafe.js_expr "printJS") [| Js.Unsafe.inject s |]
+
 let () = 
+    Sys_js.set_channel_flusher Stdlib.stdout print_to_js;
     Js.export_all
     (object%js
       method parse s =
           s |> Js.to_string |> parse_stringify |> Js.string
       method typecheckweak s =
-          let r = try
+          try
               let defs = parse_string (Js.to_string s) in
-              let res = W.print_tc (W.check_defs W.empty_ctx defs) in
-              res
-          with _ -> Printf.sprintf "Failure" in
-          Js.string r
+              W.run_tc (W.check_defs W.empty_ctx defs)
+          with _ -> Printf.printf "Failure\n%!"
       method typechecksu s =
-          let r = try
+          try
               let defs = parse_string (Js.to_string s) in
-              let res = SU.print_tc (SU.check_defs SU.empty_ctx defs) in
-              res
-          with _ -> Printf.sprintf "Failure" in
-          Js.string r
+              SU.run_tc (SU.check_defs SU.empty_ctx defs)
+          with _ -> Printf.printf "Failure\n%!"
       method typechecksua s =
-          let r = try
+          try
               let defs = parse_string (Js.to_string s) in
-              let res = SUA.print_tc (SUA.check_defs SUA.empty_ctx defs) in
-              res
-          with _ -> Printf.sprintf "Failure" in
-          Js.string r
+              SUA.run_tc (SUA.check_defs SUA.empty_ctx defs)
+          with _ -> Printf.printf "Failure\n%!"
     end)
 
